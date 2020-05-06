d96a14ea78641d585689c82724ed71db3e6822c4
ilya-protasov <pfltriot@gmail.com>
2020-05-06 19:42:25 +0300

    added choose museum screen sceleton, changed company name

--- Assets/AppAssets/Scripts/ExhibitDataSO.cs
+++ Assets/AppAssets/Scripts/ExhibitDataSO.cs
@@ -11,5 +11,8 @@ namespace AppAssets.Scripts
         [SerializeField] private string _description;
         [SerializeField] private string _hallName;
         [SerializeField] private AudioClip _audioGuid;
+        [SerializeField] private GameObject _prefab3DObject;
+
+        public bool Has3dObject => _prefab3DObject != null;
     }
 }
